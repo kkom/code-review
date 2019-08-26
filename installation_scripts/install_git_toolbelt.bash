45dadcb8fda7f92255e7f9e25ed4561745caf0ea
Konrad Komorowski <konrad@komorowski.me>
2019-08-26 14:16:16 +0200

    do not run brew upgrade during installation

--- installation_scripts/install_git_toolbelt.bash
+++ installation_scripts/install_git_toolbelt.bash
@@ -2,8 +2,6 @@
 # set strict mode
 set -euo pipefail
 
-source update_brew.bash
-
 brew install fzf moreutils
 brew tap nvie/tap
 brew install nvie/tap/git-toolbelt
--- installation_scripts/update_brew.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# set strict mode
-set -euo pipefail
-
-brew update
-brew upgrade
