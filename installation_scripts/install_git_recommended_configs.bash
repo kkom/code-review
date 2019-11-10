13bd753d50fe19c9b28627fd393bfe85137705cc
Konrad Komorowski <konrad@komorowski.me>
2019-11-10 16:14:09 +0000

    migrate bash scripts to a single makefile

--- installation_scripts/install_git_recommended_configs.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# set strict mode
-set -euo pipefail
-
-# See: https://randyfay.com/content/simpler-rebasing-avoiding-unintentional-merge-commits
-git config --global branch.autosetuprebase always
